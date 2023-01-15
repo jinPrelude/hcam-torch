264e0429e25afb39d79ce2f09ffd4fcc37240503
Euijin <jin.Prelude@gmail.com>
2023-01-15 00:28:42 +0000

    Change param set for 192 num-envs

--- cleanrl/ballet_lstm_lang_only.py
+++ cleanrl/ballet_lstm_lang_only.py
@@ -42,11 +42,11 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default='2_delay16',
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=1000000000,
+    parser.add_argument("--total-timesteps", type=int, default=100000000,
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
-    parser.add_argument("--num-envs", type=int, default=96,
+    parser.add_argument("--num-envs", type=int, default=192,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
         help="the number of steps to run in each environment per policy rollout")
@@ -58,7 +58,7 @@ def parse_args():
         help="the discount factor gamma")
     parser.add_argument("--gae-lambda", type=float, default=0.95,
         help="the lambda for the general advantage estimation")
-    parser.add_argument("--num-minibatches", type=int, default=8,
+    parser.add_argument("--num-minibatches", type=int, default=32,
         help="the number of mini-batches")
     parser.add_argument("--update-epochs", type=int, default=3,
         help="the K epochs to update the policy")
